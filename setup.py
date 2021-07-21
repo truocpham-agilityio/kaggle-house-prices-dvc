17a9834b5bfa834113e047ad430966b9bf4cbb87
Truoc Pham <truoc.phamkhac@asnet.com.vn>
2021-07-22 01:59:48 +0700

    Update locally package

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
-    name='houseprices',
+    name='src',
+    packages=find_packages(),
     version='0.1.0',
     description='Predict sales prices on the Kaggle House Prices dataset using DVC for reproducible machine learning',
     url='https://github.com/truocpham-agilityio/kaggle-house-prices-dvc',
     author='Truoc Pham',
     author_email='info@teamio.com',
     license='MIT',
-    packages=['src'],
     zip_safe=False,
     keyword=['pip', 'houseprices']
 )
